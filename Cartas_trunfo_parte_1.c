3c29c9e1137594584268a32e5ea88dc6fbfffac4
Ingryd Lorena <160596486+ingrydlorena@users.noreply.github.com>
2025-05-14 09:41:11 -0300

    Update Cartas_trunfo_parte_1.c

--- Cartas_trunfo_parte_1.c
+++ Cartas_trunfo_parte_1.c
@@ -1,67 +1,93 @@
 #include <stdio.h>
-#include <string.h>
-struct Carta {
-    //Declarando as variaveis
-    char Estado[50];
-    char Codigo[5];
-    char NomeCidade[100];
-    int Populacao;
-    float Area;
-    float Pib;
-    int NumPontosTuristicos;
-};
 
-struct Carta Baralho[2];
+int main() {
+    // Carta 1
+    char codigo1[4], estado1, nomeCidade1[50];
+    unsigned long int populacao1;
+    float area1, pib1;
+    int pontosTuristicos1;
 
-void LimparDados(char * str){
-    size_t len = strlen(str);
-    if (len > 0 && str[len-1] == '\n') {
-        str[len-1] = '\0';
-    }
-}
+    // Carta 2
+    char codigo2[4], estado2, nomeCidade2[50];
+    unsigned long int populacao2;
+    float area2, pib2;
+    int pontosTuristicos2;
 
-void LerDados(struct Carta * carta){
-    for (int i=0; i < 2; i++){
-        printf("Carta %d\n", i+1);
+    // Leitura Carta 1
+    printf("Carta 1\n");
+    printf("Codigo: ");
+    scanf("%s", codigo1);
+    printf("Estado: ");
+    scanf(" %c", &estado1);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);
+    printf("Populacao: ");
+    scanf("%lu", &populacao1);
+    printf("Area: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1);
 
-        printf("Estado: ");
-        fgets(Baralho[i].Estado,sizeof(Baralho[i].Estado), stdin);
-        LimparDados(Baralho[i].Estado);
+    // Leitura Carta 2
+    printf("\nCarta 2\n");
+    printf("Codigo: ");
+    scanf("%s", codigo2);
+    printf("Estado: ");
+    scanf(" %c", &estado2);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade2);
+    printf("Populacao: ");
+    scanf("%lu", &populacao2);
+    printf("Area: ");
+    scanf("%f", &area2);
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    printf("Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos2);
 
-        printf("Codigo: ");
-        fgets(Baralho[i].Codigo, sizeof(Baralho[i].Codigo), stdin);
-        LimparDados(Baralho[i].Codigo);
+    // Cálculos
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    float pibPerCapita1 = pib1 / populacao1;
+    float pibPerCapita2 = pib2 / populacao2;
+    float superPoder1 = populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1.0 / densidade1);
+    float superPoder2 = populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1.0 / densidade2);
 
-        printf("Nome da cidade: ");
-        fgets(Baralho[i].NomeCidade, sizeof(Baralho[i].NomeCidade), stdin);
-        LimparDados(Baralho[i].NomeCidade); 
-    
-        printf("Populacao:");
-        scanf("%d", & Baralho[i].Populacao);
-        getchar();
+    // Exibição Carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Populacao: %lu\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
-        printf("Area: ");
-        scanf("%f", & Baralho[i].Area);
-        getchar();
+    // Exibição Carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Populacao: %lu\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
-        printf("PIB: ");
-        scanf("%f", & Baralho[i].Pib);
-        getchar();
+    // Comparações
+    printf("\nComparacao de Cartas:\n");
+    printf("Populacao: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+    printf("Area: Carta 1 venceu (%d)\n", area1 > area2);
+    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
+    printf("Pontos Turisticos: Carta 1 venceu (%d)\n", pontosTuristicos1 > pontosTuristicos2);
+    printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidade1 < densidade2);
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: Carta 1 venceu (%d)\n", superPoder1 > superPoder2);
 
-        printf("Numero de pontos turisticos:");
-        scanf("%d", & Baralho[i].NumPontosTuristicos);
-        getchar();
-    }
-}
-
-void ImprimirDados(struct Carta carta){
-    printf("Carta feita!\n");
-    printf("Estado: %s\nCodigo: %s\nNome da cidade: %s\nPopulacao: %d\nArea: %.3f\nPIB: %.3f\n Numero de Pontos Turisticos:%d\n", carta.Estado, carta.Codigo, carta.NomeCidade, carta.Populacao, carta.Area, carta.Pib, carta.NumPontosTuristicos);
-
-}
-int main(){
-    struct Carta carta1;
-    LerDados(&carta1);
-    ImprimirDados(carta1);
     return 0;
 }
